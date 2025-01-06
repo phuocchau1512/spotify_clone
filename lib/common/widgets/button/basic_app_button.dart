e0636969cefc337b64e1b084fcb3147e6d41a0fb
phuocchau1512 <142923216+phuocchau1512@users.noreply.github.com>
2025-01-06 15:58:08 +0700

    theme app

---
+++ lib/common/widgets/button/basic_app_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class BasicAppButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String title;
+  final double? height;
+
+  const BasicAppButton({
+    super.key,
+    required this.onPressed,
+    required this.title,
+    this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size.fromHeight(height ?? 80), // Nếu height không được cung cấp, mặc định là 80
+      ),
+      child: Text(
+        title,
+      ),
+    );
+  }
+}
+
--- lib/presentation/intro/pages/get_started.dart
+++ lib/presentation/intro/pages/get_started.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify_clone/common/widgets/button/basic_app_button.dart';
 import 'package:spotify_clone/core/configs/assets/app_images.dart';
 import 'package:spotify_clone/core/configs/assets/app_vectors.dart';
+import 'package:spotify_clone/core/configs/theme/app_colors.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -12,6 +14,7 @@ class GetStartedPage extends StatelessWidget {
       body: Stack(
         children: [
           Container(
+            padding: const EdgeInsets.symmetric(vertical: 40,horizontal: 40),
             decoration: const BoxDecoration(
               image: DecorationImage(
                 fit: BoxFit.fill,
@@ -30,14 +33,28 @@ class GetStartedPage extends StatelessWidget {
                 ),
               ),
               const Spacer(),
-              Text(
+              const Text(
                 'Tận hưởng âm nhạc',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
                   fontSize: 18
                 ),
-              )
+              ),
+              const SizedBox(height: 21),
+              const Text(
+                'Tận hưởng âm nhạc',
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.grey,
+                    fontSize: 10,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              BasicAppButton(
+                  onPressed: (){},
+                  title: 'Bắt đầu')
             ],
           ),
           Container(
